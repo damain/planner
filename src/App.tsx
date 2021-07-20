d27c65f7415af350ddcc3d90612cbbb2ecb60190
Damain Joseph <damain@wjatt.com>
2021-07-20 14:58:55 -0400

    TailwwindCSS added and working

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
     return (
     <div className="App">
-      
+      <div className="text-2xl">Test </div>
     </div>
   )
 }
